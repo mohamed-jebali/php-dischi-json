0ac3efac186f00f3affa3be99c2da6eeaf735327
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-07-05 15:33:58 +0200

    added axios

--- script/main.js
+++ script/main.js
@@ -4,6 +4,27 @@ createApp({
   data() {
     return {
       message: 'Hello Vue!',
+      apiUrl : './server.php',
+      list:[],
     }
-  }
+  },
+  methods: {
+    axiosCall(){
+        axios.get(this.apiUrl, {
+            params: {
+
+            }
+          })
+          .then(function (response) {
+            console.log(response.data);
+            this.list = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+    }
+  },
+  created() {
+    this.axiosCall();
+},
 }).mount('#app');
